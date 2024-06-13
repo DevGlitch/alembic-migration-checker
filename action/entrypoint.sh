3554713ac617794888283ed386dfa6eba7c87a86
Luong Vo <vo.tran.thanh.luong@gmail.com>
2024-06-14 04:23:03 +0700

    feat: add support for database url (#4)
    * feat: add support for database url
    
    * Add support for db_url parameter in entrypoint.sh
    
    * Improve documentation usage example for `db_url` vs. individual parameters
    
    Also updated version tag.
    
    ---------
    
    Co-authored-by: D̷e̵v̷G̸l̶i̸t̷c̸h̶ <59583446+DevGlitch@users.noreply.github.com>

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-python /check_alembic_migration.py "${INPUT_DB_TYPE}" "${INPUT_DB_HOST}" "${INPUT_DB_PORT}" "${INPUT_DB_USER}" "${INPUT_DB_PASSWORD}" "${INPUT_DB_NAME}" "${INPUT_MIGRATIONS_PATH}"
+python /check_alembic_migration.py --db_url="${INPUT_DB_URL}" --db_type="${INPUT_DB_TYPE}" --db_host="${INPUT_DB_HOST}" --db_port="${INPUT_DB_PORT}" --db_user="${INPUT_DB_USER}" --db_password="${INPUT_DB_PASSWORD}" --db_name="${INPUT_DB_NAME}" --migrations_path="${INPUT_MIGRATIONS_PATH}"
